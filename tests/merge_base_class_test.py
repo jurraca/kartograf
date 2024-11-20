6f7a396e862791d85fd8b41b5a18f68ec45d7c57
jurraca <julienu@pm.me>
2024-11-20 20:53:11 +0000

    pull out extra file dataframe creation into function, formatting & small fixups

--- tests/merge_base_class_test.py
+++ tests/merge_base_class_test.py
@@ -2,10 +2,12 @@ import ipaddress
 import pandas as pd
 from kartograf.merge import BaseNetworkIndex
 
+
 def test_base_dict_create():
     base = BaseNetworkIndex()
     state = base.get()
     assert state == {4: {}, 6: {}}
+
 
 def test_base_dict_update():
     base = BaseNetworkIndex()
@@ -23,12 +25,8 @@ def test_check_inclusion():
     subnet = "10.10.0.0/21"
     network_int = int(ipaddress.ip_network(subnet).network_address)
     df_extra = pd.DataFrame(
-        data={
-        "INETS": network_int,
-        "ASNS": 345,
-        "PFXS": subnet,
-        "PFXS_LEADING": 10},
-        index=[0])
+        data={"INETS": network_int, "ASNS": 345, "PFXS": subnet, "PFXS_LEADING": 10},
+        index=[0],
+    )
     for row in df_extra.itertuples(index=False):
         assert base.contains_row(row)
-
